3c1db0605f469dce4230164ce4b3f8e4fea396be
Miguel Eduardo Fuentes <miguel_eduard98@hotmail.com>
2021-09-25 11:56:50 -0500

    Estructura basica del modulo de autodiagnostico

---
+++ index.js
@@ -0,0 +1,14 @@
+//Al clickear en boton inspección ejecutar esta funcion
+function inspeccion(){
+    window.location.href = "./modules/inspeccion.html";
+}
+
+//Al clickear en boton inspección covid ejecutar esta funcion
+function covid(){
+    window.location.href = "./modules/covid.html";
+}
+
+//Al clickear en boton fatiga ejecutar esta funcion
+function fatiga(){
+    window.location.href = "./modules/fatiga.html";
+}
