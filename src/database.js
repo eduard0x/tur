1be2c720af8b80ca7107eef9585efd4357c24880
eduard0x <miguel_eduard98@hotmail.com>
2021-10-20 15:09:03 -0500

    Conexión con atlas

--- src/database.js
+++ src/database.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 
 //Conexión con el cluster web alojado en Microsoft Azure - virginia
-//mongoose.connect('mongodb+srv://tur-admin:p77BGDSrzyRNWmhx@cluster0.h5031.mongodb.net/db-cucuta?retryWrites=true&w=majority');
+const direccion = 'mongodb+srv://tur-admin:p77BGDSrzyRNWmhx@cluster0.h5031.mongodb.net/tur-db?retryWrites=true&w=majority';
+mongoose.connect(direccion);
 
 //Conexión con la base de datos local
 console.log("Log: Conectando con la base de datos...");
- mongoose.connect('mongodb://localhost:27017/tur-db');
+ //mongoose.connect('mongodb://localhost:27017/tur-db');
 console.log("Log: Conexión establecida");
 
 //Crear documento en la DB
